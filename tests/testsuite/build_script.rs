9c3ba8641d6c681329f60bd7f52c4572333b94b3
Greg Morenz <greg-morenz@droid.cafe>
2024-07-05 19:25:48 -0400

    Add test verifying behavior of links_overrides with target-applies-to-host and an implicit target

--- tests/testsuite/build_script.rs
+++ tests/testsuite/build_script.rs
@@ -5777,3 +5777,78 @@ hello
 "#]])
         .run();
 }
+
+#[cargo_test]
+fn links_overrides_with_target_applies_to_host() {
+    let p = project()
+        // mylib-c is just a shared library to link against
+        .file(
+            "mylib-c/Cargo.toml",
+            r#"
+                [package]
+                name = "mylib"
+                edition = "2021"
+                version = "0.0.1"
+                authors = []
+
+                [lib]
+                crate-type = ["cdylib"]
+            "#,
+        )
+        .file(
+            "mylib-c/src/lib.rs",
+            r#"
+                #[no_mangle]
+                pub extern "C" fn foo() { println!("Hello, World!") }
+            "#,
+        )
+        .file(
+            "Cargo.toml",
+            r#"
+                [package]
+                name = "mylib-sys"
+                edition = "2021"
+                version = "0.0.1"
+                authors = []
+                links = "mylib"
+            "#,
+        )
+        .file(
+            // Use a binary that uses a symbol in mylib to force the linker to actually link it.
+            "src/main.rs",
+            r#"
+            extern "C" { fn foo(); }
+            pub fn main() { unsafe{ foo() } }
+            "#,
+        )
+        .file("build.rs", r#"fn main() { panic!("build.rs called") }"#)
+        .build();
+
+    p.cargo("build --manifest-path mylib-c/Cargo.toml")
+        .with_status(0)
+        .run();
+
+    p.cargo("build")
+        .masquerade_as_nightly_cargo(&["target-applies-to-host"])
+        .args(&[
+            "-Ztarget-applies-to-host",
+            "--config",
+            "target-applies-to-host=false",
+        ])
+        .args(&[
+            "--config",
+            &format!(r#"target.{}.mylib.rustc-link-lib=["mylib"]"#, rustc_host()),
+            "--config",
+            &format!(
+                r#"target.{}.mylib.rustc-link-search=["mylib-c/target/debug"]"#,
+                rustc_host()
+            ),
+        ])
+        .with_status(101)
+        .with_stderr_data(
+            "thread 'main' panicked at src/cargo/core/compiler/custom_build.rs:256:10:
+running a script not depending on an actual script
+...",
+        )
+        .run();
+}
