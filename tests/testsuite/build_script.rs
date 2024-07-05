277167461d3d9b51b57d57fc3408b2bd1f69510b
Greg Morenz <greg-morenz@droid.cafe>
2024-07-05 14:07:29 -0400

    Store links_overrides in Unit

--- tests/testsuite/build_script.rs
+++ tests/testsuite/build_script.rs
@@ -5844,11 +5844,10 @@ fn links_overrides_with_target_applies_to_host() {
                 rustc_host()
             ),
         ])
-        .with_status(101)
-        .with_stderr_data(
-            "thread 'main' panicked at src/cargo/core/compiler/custom_build.rs:256:10:
-running a script not depending on an actual script
-...",
-        )
+        .with_stderr_data(str![[r#"
+[COMPILING] mylib-sys v0.0.1 ([ROOT]/foo)
+[FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
+
+"#]])
         .run();
 }
